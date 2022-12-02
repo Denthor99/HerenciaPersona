e25619b270b77aeb2d849b322c057abd91405430
Denthor99 <drodsan2708@g.educaand.es>
2022-12-02 02:29:33 +0100

    Modificacion clases y herencias 01-02-2022

--- src/org/ieslosremedios/daw1/prog/herencia/Main.java
+++ src/org/ieslosremedios/daw1/prog/herencia/Main.java
@@ -6,6 +6,9 @@ public class Main {
     Persona Laura=new Persona("Laura","Ordoñez",19,"22835631-J",1020245532);
     Laura.mostrarDatos();
 
+    Persona Paco=new Persona();
+    Paco.setNombre("Pacomer");
+
     //Herencia Personal
     Personal Roberto=new Personal("Roberto","Gomez",54,"32899601-X",1050275565,"Medicina Interna",3500);
     Roberto.mostrarDatosPersonal();
